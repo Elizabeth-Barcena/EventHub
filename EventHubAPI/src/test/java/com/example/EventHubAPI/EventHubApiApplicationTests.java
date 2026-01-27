bb21b3586b796ebe2392a2f3f77378340450d34e
Elizabeth-Barcena <beth_barcena@hotmail.com>
2026-01-27 15:03:23 -0300

    chore: initial Spring Boot project setup

---
+++ EventHubAPI/src/test/java/com/example/EventHubAPI/EventHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventHubAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
